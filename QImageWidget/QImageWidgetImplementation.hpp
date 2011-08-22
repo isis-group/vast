3a6529ea14a5c97cc717a95600f5dd846a7b987a
Erik Tuerke <tuerke@cbs.mpg.de>
2011-08-22 16:45:40 +0200

    add first files for qimagewidget

---
+++ QImageWidget/QImageWidgetImplementation.hpp
@@ -0,0 +1,19 @@
+#ifndef QIMAGEWIDGETIMPLEMENTATION_HPP
+#define QIMAGEWIDGETIMPLEMENTATION_HPP
+
+#include <QImage>
+
+namespace isis {
+
+namespace viewer {
+
+namespace qt {
+    
+class QImageWidgetImplementation : public QImage
+{
+    
+};
+
+
+}}} // end namespace 
+#endif
