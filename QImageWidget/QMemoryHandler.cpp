fd1bd5143ded4db6b28d8a3ca7835cc310fa3675
Erik Tuerke <tuerke@cbs.mpg.de>
2011-09-07 15:28:32 +0200

    cosmetics

--- QImageWidget/QMemoryHandler.cpp
+++ QImageWidget/QMemoryHandler.cpp
@@ -18,26 +18,15 @@ QMemoryHandler::QMemoryHandler(QViewerCore* core)
 bool QMemoryHandler::fillSliceChunk(data::MemChunk< InternalImageType > &sliceChunk, const boost::shared_ptr< ImageHolder > image, const size_t slice, PlaneOrientation orientation) const
 {
     util::ivector4 mappedSize = QOrienationHandler::mapCoordsToOrientation( image->getImageSize(), image, orientation );
-//     switch (orientation) {
-// 	case axial:
-// 	    std::cout << "axial" << std::endl;
-// 	    break;
-// 	case sagittal:
-// 	    std::cout << "sagittal" << std::endl;
-// 	    break;
-// 	case coronal:
-// 	    std::cout << "coronal" << std::endl;
-// 	    break;
-//     }
-//     std::cout << mappedSize << std::endl;
-    util::ivector4 t = QOrienationHandler::mapCoordsToOrientation( util::ivector4(0,1,2,3), image, orientation, true );
-//     std::cout << t << std::endl;
+
+    util::ivector4 mapping = QOrienationHandler::mapCoordsToOrientation( util::ivector4(0,1,2,3), image, orientation, true );
     data::Chunk chunk = image->getChunkVector()[0];
-     for ( size_t y = 0; y < mappedSize[1]; y++ ) {
-	for ( size_t x = 0; x < mappedSize[0]; x++ ) {
-	    util::ivector4 coords (x,y,slice);
-	    sliceChunk.voxel<InternalImageType>(x,y) = 
-		chunk.voxel<InternalImageType>( coords[t[0]], coords[t[1]], coords[t[2]] );
+    util::ivector4 coords;
+    coords[2] = slice;
+     for ( coords[1] = 0; coords[1] < mappedSize[1]; coords[1]++ ) {
+	for ( coords[0] = 0; coords[0] < mappedSize[0]; coords[0]++ ) {
+	    sliceChunk.voxel<InternalImageType>(coords[0],coords[1]) = 
+		chunk.voxel<InternalImageType>( coords[mapping[0]], coords[mapping[1]], coords[mapping[2]] );
 	}
     }
 }
