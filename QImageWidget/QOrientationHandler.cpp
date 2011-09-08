2f18cf90f87bd0aa46c712c41e43796a5a8b134f
Erik Tuerke <tuerke@cbs.mpg.de>
2011-09-08 10:47:56 +0200

    some more fixes

--- QImageWidget/QOrientationHandler.cpp
+++ QImageWidget/QOrientationHandler.cpp
@@ -99,8 +99,8 @@ util::fvector4 QOrienationHandler::getScalingAndOffset(const boost::shared_ptr< 
     float normw = scalew > scaleh ? scaleh / scalew : 1;
     retVec[0] = scalew * normw;
     retVec[1] = scaleh * normh;
-    retVec[2] = ( w - retVec[0] * mappedSize[0] ) / 4 / mappedScaling[0];
-    retVec[3] = ( h - retVec[1] * mappedSize[1] ) / 4 / mappedScaling[1];
+    retVec[2] = ( w - retVec[0] * mappedSize[0] ) / 2 ;
+    retVec[3] = ( h - retVec[1] * mappedSize[1] ) / 2 ;
     return retVec;
     
 }
@@ -116,11 +116,12 @@ QTransform QOrienationHandler::getTransform(const boost::shared_ptr< ImageHolder
 			   0, 0, 1 );
     
     util::fvector4 scalingAndOffset = QOrienationHandler::getScalingAndOffset(image, w, h, orientation );
-    retTransform = retTransform.scale(scalingAndOffset[0], scalingAndOffset[1] );
-    retTransform = retTransform.translate( flipVec[0] * scalingAndOffset[2], flipVec[1] * scalingAndOffset[3] );
-    retTransform = retTransform.translate(  flipVec[0] < 0 ? -mappedSize[0] : 0, flipVec[1] < 0 ? -mappedSize[1] : 0 );
+    std::cout << scalingAndOffset << std::endl;
     
-    
+    retTransform.translate( flipVec[0] * scalingAndOffset[2], flipVec[1] * scalingAndOffset[3] );
+    retTransform.scale(scalingAndOffset[0], scalingAndOffset[1] );
+    retTransform.translate(  flipVec[0] < 0 ? -mappedSize[0] : 0, flipVec[1] < 0 ? -mappedSize[1] : 0 );
+
     return retTransform;
     
 }
