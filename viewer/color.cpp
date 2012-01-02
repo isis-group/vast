457ccfbc285116d1bd338260b52fdc0dd0ce48c5
Erik Tuerke <tuerke@cbs.mpg.de>
2012-01-02 16:30:32 +0100

    add support for hsl and hsla lut

--- viewer/color.cpp
+++ viewer/color.cpp
@@ -98,7 +98,11 @@ bool Color::addColormap( const std::string &path, const boost::regex &separator 
 			lutVec.push_back( QColor::fromHsv( colorVec[0], colorVec[1], colorVec[2], colorVec[3] ).rgba() );
         } else if ( lutTyp == std::string( "cmyk" ) ) {
             lutVec.push_back( QColor::fromCmyk( colorVec[0], colorVec[1], colorVec[2], colorVec[3] ).rgba() );            
-		} else {
+		} else if ( lutTyp == std::string( "hsl" ) ) {
+            lutVec.push_back( QColor::fromHsl( colorVec[0], colorVec[1], colorVec[2] ).rgba() );
+        } else if ( lutTyp == std::string( "hsla" ) ) {
+            lutVec.push_back( QColor::fromHsl( colorVec[0], colorVec[1], colorVec[2], colorVec[3] ).rgba() );
+        }else {
 			LOG( Runtime, warning ) << "Unknown lut type " << lutTyp << " !";
 			return false;
 		}
