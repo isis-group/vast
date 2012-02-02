7eacd254a9ba1fbdfa1275083d514a507ada06ed
lipsia-group <tuerke@cbs.mpg.de>
2012-02-02 13:34:49 +0100

    enabled about dialog

--- widgets/aboutDialog.hpp
+++ widgets/aboutDialog.hpp
@@ -42,12 +42,20 @@ namespace widget {
 class AboutDialog : public QDialog
 {
 	Q_OBJECT
+	typedef std::map<std::string, std::string> AuthorMapType;
 public:
 	AboutDialog( QWidget *parent,  QViewerCore *core);
-	
+
+public Q_SLOTS:
+    virtual void showEvent( QShowEvent *);
+    void onAuthorClicked( QString );
+    void sendEmailClicked();
+    
 private:
 	QViewerCore *m_ViewerCore;
 	Ui::aboutDialog m_Interface;
+    
+    AuthorMapType m_authorMap;
 };
 	
 	
